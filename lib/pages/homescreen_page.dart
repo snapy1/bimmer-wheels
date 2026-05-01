d8d92d11ab3a04e3810d0e0e65a6f6534949953b
snapy1 <nicholasgarman1999@gmail.com>
2026-05-01 12:49:55 -0400

    offset of banners now works

--- lib/pages/homescreen_page.dart
+++ lib/pages/homescreen_page.dart
@@ -11,7 +11,8 @@ class HomescreenPage extends StatefulWidget {
 
 class _HomescreenPageState extends State<HomescreenPage> {
   // get the screen height
-  double screenHeight(BuildContext context) => MediaQuery.of(context).size.height;
+  double screenHeight(BuildContext context) =>
+      MediaQuery.of(context).size.height;
   // get the screen width
   double screenWidth(BuildContext context) => MediaQuery.of(context).size.width;
 
@@ -19,42 +20,46 @@ class _HomescreenPageState extends State<HomescreenPage> {
     final bannerHeight = screenHeight(context);
     final stripeWidth = screenWidth(context) * 0.365;
     final stripeHeight = bannerHeight * 1.55;
+    final horizontalBannerOffset = -100.0;
     // final stripeHeight = bannerHeight * 2;
 
     return SizedBox(
       height: bannerHeight,
       child: Center(
         child: ClipRect(
-          child: SizedBox(
-            width: stripeWidth * screenWidth(context) / stripeWidth, // Ensure the banner fills the width of the screen
-            height: screenHeight(context),
-            child: Stack(
-              alignment: Alignment.center,
-              children: [
-                _buildStripe(
-                  color: Colors.blue,
-                  width: stripeWidth,
-                  height: stripeHeight,
-                  horizontalOffset: -stripeWidth,
-                  degrees: 20,
-                ),
-                _buildStripe(
-                  color: Colors.purple,
-                  width: stripeWidth,
-                  height: stripeHeight,
-                  horizontalOffset: 0,
-                  degrees: 20,
-                ),
-                _buildStripe(
-                  color: Colors.red,
-                  width: stripeWidth,
-                  height: stripeHeight,
-                  horizontalOffset: stripeWidth,
-                  degrees: 20,
-                ),
-              ],
+          child: Transform.translate(
+            offset: Offset(horizontalBannerOffset, 0),
+            child: SizedBox(
+              width: screenWidth(context),
+              height: screenHeight(context),
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  _buildStripe(
+                    color: Colors.blue,
+                    width: stripeWidth,
+                    height: stripeHeight,
+                    horizontalOffset: -stripeWidth,
+                    degrees: 20,
+                  ),
+                  _buildStripe(
+                    color: Colors.purple,
+                    width: stripeWidth,
+                    height: stripeHeight,
+                    horizontalOffset: 0,
+                    degrees: 20,
+                  ),
+                  _buildStripe(
+                    color: Colors.red,
+                    width: stripeWidth,
+                    height: stripeHeight,
+                    horizontalOffset: stripeWidth,
+                    degrees: 20,
+                  ),
+                ],
+              ),
             ),
-          ),// offset it to the left by x percentage
+          ),
         ),
       ),
     );
@@ -67,25 +72,16 @@ class _HomescreenPageState extends State<HomescreenPage> {
     required double horizontalOffset,
     required double degrees,
   }) {
+    Widget stripe = Container(width: width, height: height, color: color);
 
-    Widget stripe = Container(
-      width: width,
-      height: height,
-      color: color,
-    );
-
-    // offset the stripe 
+    // offset the stripe
     stripe = Transform.translate(
       offset: Offset(horizontalOffset, 0),
       child: stripe,
     );
 
-
     // then rotate it by N degrees
-    stripe = Transform.rotate(
-      angle: degrees * math.pi / 180,
-      child: stripe,
-    );
+    stripe = Transform.rotate(angle: degrees * math.pi / 180, child: stripe);
 
     return stripe;
   }
@@ -98,15 +94,10 @@ class _HomescreenPageState extends State<HomescreenPage> {
         child: Column(
           children: [
             // move the banner to the left X amount of percentage
-            _buildMStripeBanner(context)
-        
+            _buildMStripeBanner(context),
           ],
         ),
       ),
     );
   }
-
-
-
 }
-
