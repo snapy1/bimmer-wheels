cd198011b9e597b234199fadd4c73a0e5db223bf
snapy1 <nicholasgarman1999@gmail.com>
2026-05-01 12:56:26 -0400

    locked orientation

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,14 @@ import 'dart:convert';
 
 import 'package:bimmer_wheels/pages/initial_setup_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   runApp(const MyApp());
 }
 
